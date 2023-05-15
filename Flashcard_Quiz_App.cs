badc6ff0e4416f7eb8be41afb54c061cbce72d33
Kooreis <kooreis.official@gmail.com>
2023-05-15 04:32:56 -0500

    Created a Flashcard class with properties for Question and Answer, and a constructor to initialize these properties.

--- Flashcard_Quiz_App.cs
+++ Flashcard_Quiz_App.cs
@@ -1,2 +1,11 @@
-using System;
-using System.Collections.Generic;
+public class Flashcard
+{
+public string Question { get; set; }
+public string Answer { get; set; }
+
+public Flashcard(string question, string answer)
+{
+Question = question;
+Answer = answer;
+}
+}
